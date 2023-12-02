9bc6138b05f7f00dacf3100b9cf39d41e110107b
Vincent Lin <vinlin24@outlook.com>
2023-12-02 15:50:28 -0800

    Clean up handout source files

--- cache.cpp
+++ cache.cpp
@@ -1,25 +1,24 @@
 #include "cache.h"
 
-cache::cache()
+Cache::Cache()
 {
-	for (int i=0; i<L1_CACHE_SETS; i++)
-		L1[i].valid = false; 
-	for (int i=0; i<L2_CACHE_SETS; i++)
-		for (int j=0; j<L2_CACHE_WAYS; j++)
-			L2[i][j].valid = false; 
+    for (int i = 0; i < L1_CACHE_SETS; i++)
+        L1[i].valid = false;
+    for (int i = 0; i < L2_CACHE_SETS; i++)
+        for (int j = 0; j < L2_CACHE_WAYS; j++)
+            L2[i][j].valid = false;
 
-	// Do the same for Victim Cache ...
+    // TODO: Do the same for victim cache ...
 
-	this->myStat.missL1 =0;
-	this->myStat.missL2 =0;
-	this->myStat.accL1 =0;
-	this->myStat.accL2 =0;
+    this->myStat.missL1 = 0;
+    this->myStat.missL2 = 0;
+    this->myStat.accL1 = 0;
+    this->myStat.accL2 = 0;
 
-	// Add stat for Victim cache ... 
-	
+    // TODO: Add stat for victim cache ...
 }
-void cache::controller(bool MemR, bool MemW, int* data, int adr, int* myMem)
-{
-	// add your code here
 
+void Cache::controller(bool MemR, bool MemW, int *data, int adr, int *myMem)
+{
+    // TODO Add your code here.
 }
--- memory_driver.cpp
+++ memory_driver.cpp
@@ -1,90 +1,98 @@
+#include <cerrno>
+#include <cstring>
 #include <fstream>
 #include <iostream>
-#include <cstring>
 #include <sstream>
-#include <iostream>
 #include <vector>
+
 #include "cache.h"
+
 using namespace std;
 
-struct trace
+struct Trace
 {
-	bool MemR; 
-	bool MemW; 
-	int adr; 
-	int data; 
+    bool MemR;
+    bool MemW;
+    int adr;
+    int data;
 };
 
-/*
-Either implement your memory_controller here or use a separate .cpp/.c file for memory_controller and all the other functions inside it (e.g., LW, SW, Search, Evict, etc.)
-*/
-
-int main (int argc, char* argv[]) // the program runs like this: ./program <filename> <mode>
+static vector<Trace> readTraces(ifstream &inputFile)
 {
-	// input file (i.e., test.txt)
-	string filename = argv[1];
-	
-	ifstream fin;
+    string line;
+    vector<Trace> traces;
+    string s1, s2, s3, s4;
 
-	// opening file
-	fin.open(filename.c_str());
-	if (!fin){ // making sure the file is correctly opened
-		cout << "Error opening " << filename << endl;
-		exit(1);
-	}
-	
-	// reading the text file
-	string line;
-	vector<trace> myTrace;
-	int TraceSize = 0;
-	string s1,s2,s3,s4;
-    while( getline(fin,line) )
-      	{
-            stringstream ss(line);
-            getline(ss,s1,','); 
-            getline(ss,s2,','); 
-            getline(ss,s3,','); 
-            getline(ss,s4,',');
-            myTrace.push_back(trace()); 
-            myTrace[TraceSize].MemR = stoi(s1);
-            myTrace[TraceSize].MemW = stoi(s2);
-            myTrace[TraceSize].adr = stoi(s3);
-            myTrace[TraceSize].data = stoi(s4);
-            //cout<<myTrace[TraceSize].MemW << endl;
-            TraceSize+=1;
-        }
+    while (getline(inputFile, line))
+    {
+        stringstream ss(line);
+        getline(ss, s1, ',');
+        getline(ss, s2, ',');
+        getline(ss, s3, ',');
+        getline(ss, s4, ',');
 
+        Trace trace;
+        trace.MemR = stoi(s1);
+        trace.MemW = stoi(s2);
+        trace.adr = stoi(s3);
+        trace.data = stoi(s4);
 
-	// Defining cache and stat
-    cache myCache;
-    int myMem[MEM_SIZE]; 
+        traces.push_back(trace);
+    }
 
+    return traces;
+}
 
-	int traceCounter = 0;
-	bool cur_MemR; 
-	bool cur_MemW; 
-	int cur_adr;
-	int cur_data;
+/**
+ * USAGE: ./program FILENAME
+ */
+int main(int argc, char const *argv[])
+{
+    if (argc != 2)
+    {
+        cerr << "USAGE: " << argv[0] << " FILENAME [MODE]" << endl;
+        return EINVAL;
+    }
 
-	// this is the main loop of the code
-	while(traceCounter < TraceSize){
-		
-		cur_MemR = myTrace[traceCounter].MemR;
-		cur_MemW = myTrace[traceCounter].MemW;
-		cur_data = myTrace[traceCounter].data;
-		cur_adr = myTrace[traceCounter].adr;
-		traceCounter += 1;
-		myCache.controller (cur_MemR, cur_MemW, &cur_data, cur_adr, myMem); // in your memory controller you need to implement your FSM, LW, SW, and MM. 
-	}
-	
-	float L1_miss_rate, L2_miss_rate, AAT; 
-	//compute the stats here:
+    string inputFileName(argv[1]);
+    ifstream inputFile;
+    inputFile.open(inputFileName);
+    if (!inputFile.is_open())
+    {
+        cerr << "Error opening " << inputFileName << "." << endl;
+        return EXIT_FAILURE;
+    }
 
+    vector<Trace> traces = readTraces(inputFile);
 
-	cout<< "(" << L1_miss_rate<<","<<L2_miss_rate<<","<<AAT<<")"<<endl;
+    Cache myCache;
+    int myMem[MEM_SIZE];
 
-	// closing the file
-	fin.close();
+    int traceCounter = 0;
+    bool cur_MemR;
+    bool cur_MemW;
+    int cur_adr;
+    int cur_data;
 
-	return 0;
+    // Main program loop.
+    while (traceCounter < traces.size())
+    {
+        cur_MemR = traces[traceCounter].MemR;
+        cur_MemW = traces[traceCounter].MemW;
+        cur_data = traces[traceCounter].data;
+        cur_adr = traces[traceCounter].adr;
+        traceCounter += 1;
+        // TODO: In your memory controller you need to implement your FSM, LW,
+        // SW, and MM.
+        myCache.controller(cur_MemR, cur_MemW, &cur_data, cur_adr, myMem);
+    }
+
+    float L1_miss_rate, L2_miss_rate, AAT;
+
+    // TODO: Compute the stats here.
+
+    cout << "(" << L1_miss_rate << "," << L2_miss_rate << "," << AAT << ")"
+         << endl;
+
+    return EXIT_SUCCESS;
 }
